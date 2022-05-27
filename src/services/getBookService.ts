daaf9c3ca0980d0010687ed9af129d7fc125d72a
EduardToth <eduardtoth98@gmail.com>
2022-05-27 12:29:03 +0300

    add simpler source code

--- src/services/getBookService.ts
+++ src/services/getBookService.ts
@@ -2,7 +2,6 @@ import { Book } from "../domain";
 import { ConflictError } from "../exceptions/ConflictError";
 import { NotFoundError } from "../exceptions/NotFoundError";
 import { getRepository } from "../repositories/getRepository";
-import { executeIfNoErrorOccured } from "./helpers";
 
 export function getBookService(repository: ReturnType<typeof getRepository>) {
   const bookRepository = repository.getBookRepository();
@@ -29,45 +28,43 @@ export function getBookService(repository: ReturnType<typeof getRepository>) {
   }
 
   async function removeBookIdFromAuthor(authorId: string, bookId: string) {
-    return repository
-      .getAuthorRepository()
-      .removeBookIdFromAuthor(authorId, bookId)
-      .then((result) => (result ? result : new Error()))
-      .then((result) =>
-        executeIfNoErrorOccured<boolean, boolean>(result, (data) => data)
-      );
+    try {
+      const result = await repository
+        .getAuthorRepository()
+        .removeBookIdFromAuthor(authorId, bookId);
+
+      if (result === false) {
+        throw new Error("Unsuccessful removal");
+      }
+    } catch {
+      throw new Error("Unsuccessful removal");
+    }
   }
 
   async function addBookIdToAuthor(authorId: string, bookId: string) {
-    return repository
-      .getAuthorRepository()
-      .addBookIdToAuthor(authorId, bookId)
-      .then((result) => (result ? result : new Error()))
-      .then((result) =>
-        executeIfNoErrorOccured<boolean, boolean>(result, (data) => data)
-      );
+    try {
+      const result = await repository
+        .getAuthorRepository()
+        .addBookIdToAuthor(authorId, bookId);
+
+      if (result === false) {
+        throw new Error("Unsuccessful insertion");
+      }
+    } catch {
+      throw new Error("Unsuccessful insertion");
+    }
   }
 
   async function updateBook(id: string, book: Book) {
     // move references to new bookShelf if necessary
     const { authorId } = book;
-    /*
-    return bookRepository
-      .getBook(id)
-      .then((result) =>
-        executeIfNoErrorOccured<Book, string>(result, (data) => data.authorId)
-      )
-      .then((oldAuthorId) => removeBookIdFromAuthor(oldAuthorId, id))
-      .then(() => addBookIdToAuthor(authorId, id))
-      .then(() => bookRepository.updateBook(id, book))
-      .catch(() => new ConflictError());
-    */
+
     try {
       const oldBook = await bookRepository.getBook(id);
-      const oldAuthorId = executeIfNoErrorOccured<Book, string>(
-        oldBook,
-        (oldBook) => oldBook.authorId
-      );
+      if (oldBook instanceof Error) {
+        throw new Error();
+      }
+      const oldAuthorId = oldBook.authorId;
 
       await removeBookIdFromAuthor(oldAuthorId, id);
       await addBookIdToAuthor(authorId, id);
@@ -80,14 +77,20 @@ export function getBookService(repository: ReturnType<typeof getRepository>) {
 
   async function deleteBook(id: string) {
     //delete reference from bookshelf
-    return bookRepository
-      .getBook(id)
-      .then((result) =>
-        executeIfNoErrorOccured<Book, string>(result, (data) => data.authorId)
-      )
-      .then((authorId) => removeBookIdFromAuthor(authorId, id))
-      .then(() => bookRepository.deleteBook(id))
-      .catch(() => new NotFoundError());
+    try {
+      const book = await bookRepository.getBook(id);
+
+      if (book instanceof Error) {
+        throw new Error();
+      }
+
+      const { authorId } = book;
+      await removeBookIdFromAuthor(authorId, id);
+
+      return bookRepository.deleteBook(id);
+    } catch {
+      return new NotFoundError();
+    }
   }
 
   return { getBook, createBook, getAllBooks, updateBook, deleteBook };
